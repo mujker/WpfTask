5859fedb86ea4e4565fd2b82a4112b9eda3e1ef8
Administrator <Administrator@X6X8-20170408ZB>
2017-08-24 22:25:50 +0800

    Task并发开始, 停止测试 结论, 不建议强制 Dispose Parallel 并发 停止 用 loopstate break

--- WpfTaskTest/MainWindow.xaml.cs
+++ WpfTaskTest/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Security.AccessControl;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfTaskTest
 {
@@ -20,9 +12,81 @@ namespace WpfTaskTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        static CancellationTokenSource tokenSource = new CancellationTokenSource();
+
+        // create the cancellation token
+        static CancellationToken token = tokenSource.Token;
+        private Task _taskMain;
+
+        private bool _taskFlag;
+
+        private List<int> mList = new List<int>();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            for (int i = 0; i < 200; i++)
+            {
+                mList.Add(i);
+            }
+        }
+
+        private void Button1_OnClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("开始");
+            StartFunc();
+        }
+
+        private void Button2_OnClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("结束");
+            tokenSource.Cancel();
+            StopFunc();
+        }
+
+        /// <summary>
+        /// 开始
+        /// </summary>
+        private void StartFunc()
+        {
+            _taskFlag = true;
+            _taskMain = Task.Factory.StartNew(delegate
+            {
+                Parallel.For(0, mList.Count, (i, loopState) =>
+                {
+                    if (!_taskFlag)
+                    {
+                        loopState.Break();
+                    }
+                    TaskFunc(i);
+                });
+//                for (int i = 0; i < 200; i++)
+//                {
+//                    var i1 = i;
+//                    Task.Factory.StartNew(delegate
+//                    {
+//                        TaskFunc(i1);
+//                    }, token);
+//                }
+            });
+        }
+
+        /// <summary>
+        /// 结束
+        /// </summary>
+        private void StopFunc()
+        {
+            _taskFlag = false;
+        }
+
+        private void TaskFunc(int i)
+        {
+            while (_taskFlag)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
         }
     }
 }
