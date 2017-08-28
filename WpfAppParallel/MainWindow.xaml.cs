47a93ca9870558e382eedebcc5670e1bde877212
Administrator <Administrator@X6X8-20170408ZB>
2017-08-28 20:52:05 +0800

    并发千个线程测试

---
+++ WpfAppParallel/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAppParallel
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<MyClass> MyList = new List<MyClass>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PushList(); //填充数据
+            StartWork(); //并发任务
+        }
+
+        private void StartWork()
+        {
+            Parallel.ForEach(MyList, item =>
+            {
+                Task.Factory.StartNew(delegate
+                {
+                    while (true)
+                    {
+                        item.Sj = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                        Thread.Sleep(1000);//防止程序卡死
+                    }
+                }, TaskCreationOptions.LongRunning);
+                //https://technet.microsoft.com/zh-CN/library/system.threading.tasks.taskcreationoptions
+            });
+        }
+
+        private void PushList()
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                MyList.Add(new MyClass() {Id = i.ToString(), Name = "Parallel" + i});
+            }
+            MainGrid.ItemsSource = MyList;
+        }
+    }
+}
---
+++ WpfAppParallel/MyClass.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppParallel
+{
+    public class MyClass : INotifyPropertyChanged
+    {
+
+        private string _id;
+        private string _name;
+        private string _sj;
+
+        public string Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Sj
+        {
+            get
+            {
+                return _sj;
+            }
+
+            set
+            {
+                _sj = value;
+                OnPropertyChanged("Sj");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
